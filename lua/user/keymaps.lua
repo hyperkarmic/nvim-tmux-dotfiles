217726a675a4374ce99d7bba911d2ce499dd7729
hyperkarmic <randy_pan@yahoo.com>
2022-11-28 18:47:02 +0000

    move lines implemented in keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,55 +1,55 @@
 
 local keymap = vim.keymap.set
--- Silent keymap option
 local opts = { silent = true }
+
+-- Silent keymap option
 
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 
--- Modes(ie what the first keymap argument correspands to!!!)
---   normal_mode = "n",
---   insert_mode = "i",
---   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
-
 -- Normal --
 --" Shortcut to use blackhole register by default
-keymap( 'n','d' ,'"_d',opts )
-keymap( 'v','d' ,'"_d',opts )
-keymap( 'n', 'D', '"_D',opts )
-keymap( 'v', 'D', '"_D',opts )
-keymap( 'n', 'c', '"_c',opts )
-keymap( 'v', 'c', '"_c',opts )
-keymap( 'n', 'C', '"_C',opts )
-keymap( 'v', 'C', '"_C',opts )
-keymap( 'n', 'x', '"_x',opts )
-keymap( 'v', 'x', '"_x',opts )
-keymap( 'n', 'X', '"_X',opts )
-keymap( 'v', 'X', '"_X',opts )
-
--- Shortcut to use clipboard with <leader>
-keymap( "n",';d','d',opts )
-keymap( "v",';d','d',opts )
-keymap( "n",';D','D',opts )
-keymap( "v",';D','D',opts )
-keymap( "n",';c','c',opts )
-keymap( "v",';c','c',opts )
-keymap( "n",';C','C',opts )
-keymap( "v",';C','C',opts )
-keymap( "n",';x','x',opts )
-keymap( "v",';x','x',opts )
-keymap( "n",';X','X',opts )
-keymap( "v",';X','X',opts )
-
+-- keymap( 'n','d' ,'"_d',opts )
+-- keymap( 'v','d' ,'"_d',opts )
+-- keymap( 'n', 'D', '"_D',opts )
+-- keymap( 'v', 'D', '"_D',opts )
+-- keymap( 'n', 'c', '"_c',opts )
+-- keymap( 'v', 'c', '"_c',opts )
+-- keymap( 'n', 'C', '"_C',opts )
+-- keymap( 'v', 'C', '"_C',opts )
+-- keymap( 'n', 'x', '"_x',opts )
+-- keymap( 'v', 'x', '"_x',opts )
+-- keymap( 'n', 'X', '"_X',opts )
+-- keymap( 'v', 'X', '"_X',opts )
+--
+-- -- Shortcut to use clipboard with <leader>
+-- keymap( "n",';d','d',opts )
+-- keymap( "v",';d','d',opts )
+-- keymap( "n",';D','D',opts )
+-- keymap( "v",';D','D',opts )
+-- keymap( "n",';c','c',opts )
+-- keymap( "v",';c','c',opts )
+-- keymap( "n",';C','C',opts )
+-- keymap( "v",';C','C',opts )
+-- keymap( "n",';x','x',opts )
+-- keymap( "v",';x','x',opts )
+-- keymap( "n",';X','X',opts )
+-- keymap( "v",';X','X',opts )
 
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+
+--move line
+keymap("n","<M-Down>",":m .+1<CR>==",opts)
+keymap("v","<M-Down>",":m '>+1<CR>gv=gv",opts)
+keymap("i","<M-Down>","<ESC>:m .+1<CR>==gi",opts)
+keymap("n","<M-Up>",":m .-2<CR>==",opts)
+keymap("v","<M-Up>",":m '<-2<CR>gv=gv",opts)
+keymap("i","<M-Up>","<ESC>:m .-2<CR>==gi",opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
@@ -66,9 +66,6 @@ keymap("n", "]B", ":blast<CR>", opts)
  keymap("n","<C-q>",":bd<Cr>",opts)
      --buffer back
 keymap("n", "<leader>b", "<c-^>",opts )
-
-
-
 
 -- Clear highlights
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
@@ -125,7 +122,7 @@ keymap('t', '<leader>zz', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', o
 
 
 --insert mode big D delete!!!!!
-keymap("i", "<C-d>","<C-O>D",opts)
+--keymap("i", "<C-d>","<C-O>D",opts)
 
 --toggle spellcheck
 local function vim_opt_toggle(opt, on, off, name)
@@ -152,3 +149,4 @@ keymap('n', '<leader>ww', "<cmd>lua require'hop'.hint_char1()<cr>", opts)
 --vim.api.nvim_set_keymap('', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false })<cr>", {})
 --vim.api.nvim_set_keymap('', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false, hint_offset = -1 })<cr>", {})
 -- vim.api.nvim_set_keymap('', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })<cr>", {})
+
